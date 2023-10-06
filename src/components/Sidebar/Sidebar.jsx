682c95011c3fa9ebfbd884fba0db320ead72f8a1
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-10-06 22:42:48 +0530

    added sidebar order

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import './Sidebar.css';
 export const Sidebar = () => {
   return (
-    <div >
-      <ul>
+    <div className='sidebar-container'>
+      <ul className='sidebar-category'>
         <li>All</li>
         <li>Favourite</li>
         <li>Archieved</li>
         <li>Trash</li>
       </ul>
+      <span className='sidebar-datewise d-flex j-center dir-col'>Sorting Order
+      <label value='order'><input type='radio' id='order'/>   Sort By Newer</label>
+      <label value='order'><input type='radio' id='order'/>   Sort By Older</label>
+      </span>
     </div>
   )
 }
