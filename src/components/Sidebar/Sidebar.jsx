5324ce3915529b5c949cc3fcc3c1c23d86ff0ddc
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-10-07 20:51:35 +0530

    added sidebar css

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,12 +10,12 @@ export const Sidebar = () => {
         <button className='sidebar-cat-btn'>Trash</button>
       </span>
 
-      <span className='sidebar-datewise d-flex j-center dir-col'>Sorting Order
+      <span className='sidebar-datewise d-flex j-center dir-col pad-s'>Sorting Order
       <label value='order-new'><input type='radio' id='order-new' name='order'/>   Sort By Newer</label>
       <label value='order-old'><input type='radio' id='order-old' name='order'/>   Sort By Older</label>
       </span>
 
-      <span className='sidebar-cateorywise d-flex j-center dir-col'>Cateory
+      <span className='sidebar-cateorywise d-flex j-center dir-col pad-s'>Category
       <label value='all'><input type='radio' id='all' name='category'/>   All</label>
       <label value='work'><input type='radio' id='work' name='category'/>   Work</label>
       <label value='personal'><input type='radio' id='personal' name='category'/>   Pesronal</label>
@@ -23,12 +23,12 @@ export const Sidebar = () => {
       <label value='education'><input type='radio' id='education' name='category'/>   Education</label>
       </span>
 
-      <span className='sidebar-prioritywise d-flex j-center dir-col'>Cateory
+      <span className='sidebar-prioritywise d-flex j-center dir-col pad'>Priority
       <label value='all'><input type='radio' id='all' name='priority'/>   All</label>
       <label value='low'><input type='radio' id='low' name='priority'/>   Low</label>
       <label value='medium'><input type='radio' id='medium' name='priority'/>   Medium</label>
       <label value='high'><input type='radio' id='high' name='priority'/>   High</label>
-      
+
       </span>
     </div>
   )
