b574060d5d1c25861fd814a321d6445fee4cc8f3
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-10-08 20:13:30 +0530

    added css for notes

--- src/components/todocard/todocard.jsx
+++ src/components/todocard/todocard.jsx
@@ -8,11 +8,11 @@ export const Todocard = () => {
         <section className="todo-card">
         {todoList.map(({ title, task, category, priority }, index) => {
           return (
-            <div key={index} className="todo-cards">
-              <h1>{title}</h1>
-              <p>{task}</p>
-              <p>{category}</p>
-              <p>{priority}</p>
+            <div key={index} className="todo-cards ">
+              <h3 className='todo-components'>Title:  <span className='todo-component'>{title}</span></h3>
+              <h3 className='todo-components'>Cateory:<span  className='todo-component'>{category}</span></h3>
+              <h3 className='todo-components'>Priority:<span  className='todo-component'>{priority}</span></h3>
+              <h3 className='todo-components'>Note:  <span className='todo-note'>{task}</span></h3>
             </div>
           );
         })}
